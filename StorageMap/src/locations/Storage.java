ad60e9fe616335de2e21beb4ef64051be3f4f3e1
Jurrunio <114945137+aleung29@users.noreply.github.com>
2022-11-07 17:02:26 -0800

    11/7 Update 1
    Check is done during store function for identical barcodes.

--- StorageMap/src/locations/Storage.java
+++ StorageMap/src/locations/Storage.java
@@ -3,7 +3,7 @@ package locations;
 public interface Storage {
     //Format of spots: x-coor (length), y-coor (width), z-coor (height)
     void list();
-    void store();
+    void store(String s);
     boolean find(String l, String s);
     void locate(String l, String s);
     void take(String l, String s);
